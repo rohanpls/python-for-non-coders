08d639e3bc25e1f2a98160fdfdad2513b0463de6
rohanpls <rnsh147@gmail.com>
2022-06-03 15:27:53 +0530

    Assigning boolean value to variables

--- boolean_and_conditions.py
+++ boolean_and_conditions.py
@@ -6,3 +6,14 @@ if feeling_hungry:  #if variable has boolean value true
     print('feeling super hungry right now')
 else:   #if boolean value is false then execute the following
     print('I don\'t feel hungry right now')
+
+age = 23
+
+checkAge = age > 20  #assigning boolean value to a variable
+
+#comparing two things in python turns into a boolean
+
+if checkAge:
+    print('You should maintain your diet')
+else:
+    print('You should eat more healthy because you are growing')
