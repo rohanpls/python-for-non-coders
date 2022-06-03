f8d829d87fd8ad91769396e2febb7ec753d82a58
rohanpls <rnsh147@gmail.com>
2022-06-03 15:20:57 +0530

    Using Booleans with conditions in python

---
+++ boolean_and_conditions.py
@@ -0,0 +1,8 @@
+#Example on how boolean works in python and how you can use it in conditions
+
+feeling_hungry = True
+
+if feeling_hungry:  #if variable has boolean value true
+    print('feeling super hungry right now')
+else:   #if boolean value is false then execute the following
+    print('I don\'t feel hungry right now')
