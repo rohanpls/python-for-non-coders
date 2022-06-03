423fcdeed252ad86bcf474bf514b7e38d02ccfd2
rohanpls <rnsh147@gmail.com>
2022-06-03 14:39:28 +0530

    using variables in strings

---
+++ variables_in_strings.py
@@ -0,0 +1,6 @@
+#Example on how you can use variables within strings while using print()
+name = 'Rohan'
+age = 23
+print(f'Hi my name is {name} and I\'m {age} years old.')
+
+#adding 'f' will tell the statement to print variables, you can place the variables using {}
