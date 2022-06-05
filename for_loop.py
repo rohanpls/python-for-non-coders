b6259120921c9e948f05085f6087131fc2e49cb1
rohanpls <rnsh147@gmail.com>
2022-06-05 13:37:46 +0530

    Using For Loop in python

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+# THIS CODE WILL TELL YOU HOW TO USE FOR LOOPs
+
+for i in range(5):   # i is loop variable like you use in C/C++ to run it 'i' number of times.
+    #range is number of times you want the loop to repeat
+    print(i)
+    
+ #IT IS IDEAL TO NAME LOOP VARIABLE TO SOMETHING WHICH IS EASIER TO MAKE SENSE OF
+ 
+for number in range(10):
+     print(number)
+ 
+ # Above code makes more sense to read
+    
+
+#how to use for loop with array    
+social_media = ['Twitter', 'Instagram', 'Facebook', 'Snapchat']
+
+for social in social_media:
+    print(f'I use {social}!')
+    
+# Social variable is assigned the array value instead of ascending number
+    
