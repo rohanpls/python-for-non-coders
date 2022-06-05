dcb7fd8c9ccceb9e7b639db79ccd825c86229cd7
rohanpls <rnsh147@gmail.com>
2022-06-05 05:53:17 +0530

    How to intialise arrays

---
+++ initialise_arrays.py
@@ -0,0 +1,5 @@
+array_name = ["value1", "value2",'value3', 4, 5.0] # Declare the array with some values, even mixed ones.
+
+print(array_name[0]) #Print the array value
+
+print(f'This will tell you if you can extract array value: {array_name[4]}') #you can insert any variable or array here.
