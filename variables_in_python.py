fff696cea80953462cb3fc8ec67b0d94bfb9dea5
rohanpls <rnsh147@gmail.com>
2022-06-09 08:56:13 +0530

    Declare variables in python

---
+++ variables_in_python.py
@@ -0,0 +1,16 @@
+#How you can declare variables in python
+number = 147
+print(number)#Declare an int
+print(type(number))
+
+isTrue = True #Declare a boolean
+print(isTrue)
+print(type(isTrue))
+
+floatValue = 147.00 #Declare a float value
+print(floatValue)
+print(type(floatValue))
+
+myName = 'Rohan'  #Declare a string
+print(myName)
+print(type(myName))
