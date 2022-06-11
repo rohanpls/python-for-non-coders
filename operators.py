4b5be2aedb82a59a6f6f7dfb54e98a8ca63143a6
rohanpls <rnsh147@gmail.com>
2022-06-11 20:31:34 +0530

    Operators in Python

---
+++ operators.py
@@ -0,0 +1,26 @@
+firstNumber = 10
+
+print(firstNumber)
+
+secondNumber = firstNumber + 15 #Addition
+
+print(secondNumber)
+
+thirdNumber = secondNumber - 15 #Subtract
+
+print(thirdNumber)
+
+fourthNumber = thirdNumber * 2 #Multiply
+
+print(fourthNumber)
+
+fifthNumber = fourthNumber / 2 #Quotient
+
+sixthNumber = fourthNumber % 2 #Remainder
+
+print(fifthNumber)
+print(sixthNumber)
+
+calculation = ((firstNumber+secondNumber)-thirdNumber)*fourthNumber
+
+print(calculation)
